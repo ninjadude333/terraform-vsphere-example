ad438b1631c3d6bbfedf0c9d6b177b484d95f176
David Gidony <david.gidony@gmail.com>
2023-08-01 12:19:30 +0300

    tf example for vsphere

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # terraform-vsphere-example
+
+This is an example terraform project for deploying VM templates on Vsphere.
--- variables.tf
+++ variables.tf
@@ -1,41 +1,55 @@
 variable "ipv4_address" {
-  type = string
+  type        = string
+  description = "IP adress to be configured on the VM"
 }
 
 variable "ipv4_mask" {
-  type    = number
-  default = 24
+  type        = number
+  default     = 24
+  description = "IP mask class to be configured on the VM"
 }
 
 variable "ipv4_gw" {
-  type    = string
-  default = "172.16.204.1"
+  type        = string
+  description = "Default Gateway address to be configured on the VM"
+}
+
+variable "dns_server_list" {
+  type        = list(string)
+  description = "DNS Server list to be configured on new VM"
 }
 
 variable "computer_name" {
-  type = string
+  type        = string
+  description = "Computer name to be configured on the VM"
 }
 
 variable "vmName" {
-  type = string
+  type        = string
+  description = "VM name for the VM object to be deployed"
 }
 
 variable "vmFolder" {
-  type = string
+  type        = string
+  description = "Folder in Vsphere to deploy the VM, and fetch template from"
 }
 
 variable "vmTemplate" {
-  type = string
+  type        = string
+  description = "VM Template Name"
 }
 
 variable "vsphereUser" {
-  type = string
+  type        = string
+  description = "Vsphere user name for vshpere login"
 }
 
 variable "vspherePass" {
-  type = string
+  type        = string
+  description = "Vsphere password for vsphere login"
 }
 
 variable "vsphereAddr" {
-  type = string
+  type        = string
+  description = "url/address of vsphere server"
 }
